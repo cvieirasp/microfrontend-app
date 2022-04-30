1d599e6bacfe3bb721ca1dccef64192a8f583583
Carlos Figueiredo <cvieirasp@gmail.com>
2022-04-30 18:20:13 -0300

    added header and randomize production class names

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 
+import Header from './components/Header';
 import MarketingApp from './components/MarketingApp';
 
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'co'
+});
+
 export default () => {
-  return <div>
-    <MarketingApp />
-  </div>;
+  return (
+    <BrowserRouter>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </StylesProvider>
+    </BrowserRouter>
+  );
 };
